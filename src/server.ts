70c8d42af43c930fdf10e2514631e75148b354b4
Vitor Souza <vhsouzaeng@gmail.com>
2021-04-24 20:08:45 -0300

    feat: creating docs

--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,21 @@ import "./shared/containers";
 import chalk from "chalk";
 import cors from "cors";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { port, env } from "./config/environment";
 import uploadConfig from "./config/upload";
 import GlobalErrorMiddleware from "./shared/middlewares/GlobalErrorMiddleware";
 import routes from "./shared/routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
 app.use(GlobalErrorMiddleware);
